09bad1918dbdd8845113f329b8f48a0dac70b1f0
Sergei Dudka <dudk@uber.com>
2020-02-28 17:04:04 +0100

    switch pool to use pointers

--- pool/pool.go
+++ pool/pool.go
@@ -11,33 +11,34 @@ import (
 type Pool struct {
 	bufferSize  int
 	numChannels int
-	pool        *sync.Pool
+	pool        sync.Pool
 }
 
 // New returns new pool.
-func New(numChannels, bufferSize int) Pool {
-	return Pool{
+func New(numChannels, bufferSize int) *Pool {
+	return &Pool{
 		bufferSize:  bufferSize,
 		numChannels: numChannels,
-		pool: &sync.Pool{
+		pool: sync.Pool{
 			New: func() interface{} {
-				return signal.Float64Buffer(numChannels, bufferSize)
+				b := signal.Float64Buffer(numChannels, bufferSize)
+				return &b
 			},
 		},
 	}
 }
 
 // Alloc retrieves new signal.Float64 buffer from the pool.
-func (p Pool) Alloc() signal.Float64 {
-	return p.pool.Get().(signal.Float64)
+func (p *Pool) Alloc() *signal.Float64 {
+	return p.pool.Get().(*signal.Float64)
 }
 
 // Free returns signal.Float64 buffer to the pool. Buffer is also cleared up.
-func (p Pool) Free(b signal.Float64) {
+func (p *Pool) Free(b *signal.Float64) {
 	if b.NumChannels() == p.numChannels && b.Size() == p.bufferSize {
-		for i := range b {
-			for j := range b[i] {
-				b[i][j] = 0
+		for i := range *b {
+			for j := range (*b)[i] {
+				(*b)[i][j] = 0
 			}
 		}
 		p.pool.Put(b)
