87776479180d6424a9ec0cd7179080f4c44ac589
Sergei Dudka <serg.dudk@gmail.com>
2020-04-06 18:34:28 +0200

    rearrange code for int64

--- int64.go
+++ int64.go
@@ -37,27 +37,7 @@ func (a Allocator) Int64(bd BitDepth) Int64 {
 	}
 }
 
-// Int64Interleaved returns new int64 interleaved buffer. If non-nill parameter
-// is provided, the values will a. copied into result buffer. Result buffer will
-// always have size provided a. properties.
-func (a Allocator) Int64Interleaved(bd BitDepth) Int64Interleaved {
-	if bd == 0 || bd == MaxBitDepth {
-		bd = BitDepth64
-	}
-	return Int64Interleaved{
-		buffer:   make([]int64, a.Capacity*a.Channels),
-		capacity: capacity(a.Capacity),
-		channels: channels(a.Channels),
-		bitDepth: bitDepth(bd),
-		length:   &length{},
-	}
-}
-
 func (f Int64) MaxBitDepth() BitDepth {
-	return BitDepth64
-}
-
-func (f Int64Interleaved) MaxBitDepth() BitDepth {
 	return BitDepth64
 }
 
@@ -65,19 +45,17 @@ func (f Int64) Sample(channel, pos int) int64 {
 	return f.buffer[channel][pos]
 }
 
-func (f Int64Interleaved) Sample(channel, pos int) int64 {
-	return f.buffer[interPos(f.Channels(), channel, pos)]
+func (f Int64) Data() [][]int64 {
+	return f.buffer
+}
+
+func (f Int64) setSample(channel, pos int, val int64) {
+	f.buffer[channel][pos] = val
 }
 
 // Append appends [0:Length] data from src to current buffer and returns new buffer.
 func (f Int64) Append(src Int64) Int64 {
 	mustSameChannels(f.Channels(), f.Channels())
-	mustSameBitDepth(f.BitDepth(), src.BitDepth())
-	panic("not implemented")
-}
-
-func (f Int64Interleaved) Append(src Int64Interleaved) Int64Interleaved {
-	mustSameChannels(f.Channels(), src.Channels())
 	mustSameBitDepth(f.BitDepth(), src.BitDepth())
 	panic("not implemented")
 }
@@ -121,25 +99,47 @@ func (f Int64) WriteInt(ints [][]int) int {
 	return copied
 }
 
-func (f Int64Interleaved) WriteInt64(ints []int64) int {
-	bufLen := f.Length() * f.Channels()
-	n := copy(f.buffer[bufLen:], ints)
-	f.setLength(interLen(f.Channels(), bufLen+n))
-	return n
+// Int64Interleaved returns new int64 interleaved buffer. If non-nill parameter
+// is provided, the values will a. copied into result buffer. Result buffer will
+// always have size provided a. properties.
+func (a Allocator) Int64Interleaved(bd BitDepth) Int64Interleaved {
+	if bd == 0 || bd == MaxBitDepth {
+		bd = BitDepth64
+	}
+	return Int64Interleaved{
+		buffer:   make([]int64, a.Capacity*a.Channels),
+		capacity: capacity(a.Capacity),
+		channels: channels(a.Channels),
+		bitDepth: bitDepth(bd),
+		length:   &length{},
+	}
 }
 
-func (f Int64) Data() [][]int64 {
-	return f.buffer
+func (f Int64Interleaved) MaxBitDepth() BitDepth {
+	return BitDepth64
+}
+
+func (f Int64Interleaved) Sample(channel, pos int) int64 {
+	return f.buffer[interPos(f.Channels(), channel, pos)]
 }
 
 func (f Int64Interleaved) Data() []int64 {
 	return f.buffer
 }
 
-func (f Int64) setSample(channel, pos int, val int64) {
-	f.buffer[channel][pos] = val
-}
-
 func (f Int64Interleaved) setSample(channel, pos int, val int64) {
 	f.buffer[interPos(f.Channels(), channel, pos)] = val
+}
+
+func (f Int64Interleaved) Append(src Int64Interleaved) Int64Interleaved {
+	mustSameChannels(f.Channels(), src.Channels())
+	mustSameBitDepth(f.BitDepth(), src.BitDepth())
+	panic("not implemented")
+}
+
+func (f Int64Interleaved) WriteInt64(ints []int64) int {
+	bufLen := f.Length() * f.Channels()
+	n := copy(f.buffer[bufLen:], ints)
+	f.setLength(interLen(f.Channels(), bufLen+n))
+	return n
 }
