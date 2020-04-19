553ebf71ecb427265ec5af760904c3ec467fb6b0
Sergei Dudka <serg.dudk@gmail.com>
2020-04-19 23:07:08 +0200

    fix formatting and rename reciever

--- int64.go
+++ int64.go
@@ -1,15 +1,13 @@
 package signal
 
-type (
-	// Int64 is a sequential int64 signal.
-	Int64 struct {
-		buffer []int64
-		capacity
-		channels
-		bitDepth
-		length
-	}
-)
+// Int64 is int64 signed fixed signal.
+type Int64 struct {
+	buffer []int64
+	capacity
+	channels
+	bitDepth
+	length
+}
 
 // Int64 allocates new sequential int64 signal buffer.
 func (a Allocator) Int64(bd BitDepth) Int64 {
@@ -22,22 +20,22 @@ func (a Allocator) Int64(bd BitDepth) Int64 {
 }
 
 // MaxBitDepth returns maximal bit depth for this type.
-func (f Int64) MaxBitDepth() BitDepth {
+func (s Int64) MaxBitDepth() BitDepth {
 	return BitDepth64
 }
 
 // Sample returns signal value for provided channel and position.
-func (f Int64) Sample(channel, pos int) int64 {
-	return f.buffer[interPos(f.Channels(), channel, pos)]
+func (s Int64) Sample(channel, pos int) int64 {
+	return s.buffer[interPos(s.Channels(), channel, pos)]
 }
 
-func (f Int64) setSample(channel, pos int, val int64) {
-	f.buffer[interPos(f.Channels(), channel, pos)] = val
+func (s Int64) setSample(channel, pos int, val int64) {
+	s.buffer[interPos(s.Channels(), channel, pos)] = val
 }
 
-func (f Int64) setLength(l int) Signed {
-	f.length = length(l)
-	return f
+func (s Int64) setLength(l int) Signed {
+	s.length = length(l)
+	return s
 }
 
 // Append appends [0:Length] data from src to current buffer and returns new
@@ -45,19 +43,19 @@ func (f Int64) setLength(l int) Signed {
 // otherwise function will panic. If current buffer doesn't have enough capacity,
 // additional memory will be allocated and result buffer will have capacity and
 // length equal to sum of lengths.
-func (f Int64) Append(src Signed) Signed {
-	mustSameChannels(f.Channels(), src.Channels())
-	mustSameBitDepth(f.BitDepth(), src.BitDepth())
-	newLen := f.Length() + src.Length()
-	if f.Capacity() < newLen {
-		f.buffer = append(f.buffer, make([]int64, (newLen-f.Capacity())*f.Channels())...)
-		f.capacity = capacity(newLen)
+func (s Int64) Append(src Signed) Signed {
+	mustSameChannels(s.Channels(), src.Channels())
+	mustSameBitDepth(s.BitDepth(), src.BitDepth())
+	newLen := s.Length() + src.Length()
+	if s.Capacity() < newLen {
+		s.buffer = append(s.buffer, make([]int64, (newLen-s.Capacity())*s.Channels())...)
+		s.capacity = capacity(newLen)
 	}
-	for channel := 0; channel < f.Channels(); channel++ {
+	for channel := 0; channel < s.Channels(); channel++ {
 		for pos := 0; pos < src.Length(); pos++ {
-			f.setSample(channel, pos+f.Length(), src.Sample(channel, pos))
+			s.setSample(channel, pos+s.Length(), src.Sample(channel, pos))
 		}
 	}
-	f.length = length(newLen)
-	return f
+	s.length = length(newLen)
+	return s
 }
