c155e041fe4f1577441466915f24b1290c6c175c
Sergei Dudka <dudk@uber.com>
2020-04-19 23:07:34 +0200

    fix formatting and rename reciever

--- float64.go
+++ float64.go
@@ -17,34 +17,34 @@ func (a Allocator) Float64() Float64 {
 	}
 }
 
-func (f Float64) setLength(l int) Floating {
-	f.length = length(l)
-	return f
+func (s Float64) setLength(l int) Floating {
+	s.length = length(l)
+	return s
 }
 
 // Sample returns signal value for provided channel and position.
-func (f Float64) Sample(channel, pos int) float64 {
-	return f.buffer[interPos(f.Channels(), channel, pos)]
+func (s Float64) Sample(channel, pos int) float64 {
+	return s.buffer[interPos(s.Channels(), channel, pos)]
 }
 
-func (f Float64) setSample(channel, pos int, value float64) {
-	f.buffer[interPos(f.Channels(), channel, pos)] = value
+func (s Float64) setSample(channel, pos int, value float64) {
+	s.buffer[interPos(s.Channels(), channel, pos)] = value
 }
 
 // Append appends data from src buffer to the end of the buffer.
 // The result buffer has capacity and length equal to sum of lengths.
-func (f Float64) Append(src Floating) Floating {
-	mustSameChannels(f.Channels(), src.Channels())
-	newLen := f.Length() + src.Length()
-	if f.Capacity() < newLen {
-		f.buffer = append(f.buffer, make([]float64, (newLen-f.Capacity())*f.Channels())...)
-		f.capacity = capacity(newLen)
+func (s Float64) Append(src Floating) Floating {
+	mustSameChannels(s.Channels(), src.Channels())
+	newLen := s.Length() + src.Length()
+	if s.Capacity() < newLen {
+		s.buffer = append(s.buffer, make([]float64, (newLen-s.Capacity())*s.Channels())...)
+		s.capacity = capacity(newLen)
 	}
-	for channel := 0; channel < f.Channels(); channel++ {
+	for channel := 0; channel < s.Channels(); channel++ {
 		for pos := 0; pos < src.Length(); pos++ {
-			f.setSample(channel, pos+f.Length(), src.Sample(channel, pos))
+			s.setSample(channel, pos+s.Length(), src.Sample(channel, pos))
 		}
 	}
-	f.length = length(newLen)
-	return f
+	s.length = length(newLen)
+	return s
 }
