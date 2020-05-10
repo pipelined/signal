f13b362ed0ae3b23a78685802dd3aa7bd98ee4ec
Sergei Dudka <dudk@uber.com>
2020-05-10 13:56:45 +0200

    add uint64 usigned type

--- uint64.go
+++ uint64.go
@@ -1,19 +1,16 @@
 package signal
 
-// Uint64 is uint64 unsigned fixed signal.
+// Uint64 is uint64 signed fixed signal.
 type Uint64 struct {
 	buffer []uint64
-	capacity
 	channels
 	bitDepth
-	length
 }
 
-// Uint64 allocates new uint64 signal buffer.
+// Uint64 allocates new sequential uint64 signal buffer.
 func (a Allocator) Uint64(bd BitDepth) Uint64 {
 	return Uint64{
-		buffer:   make([]uint64, a.Channels*a.Capacity),
-		capacity: capacity(a.Capacity),
+		buffer:   make([]uint64, 0, a.Capacity*a.Channels),
 		channels: channels(a.Channels),
 		bitDepth: bd.cap(BitDepth64),
 	}
@@ -24,44 +21,115 @@ func (s Uint64) MaxBitDepth() BitDepth {
 	return BitDepth64
 }
 
-// Sample returns signal value for provided channel and position.
-func (s Uint64) Sample(channel, pos int) uint64 {
-	return s.buffer[interPos(s.Channels(), channel, pos)]
+func (s Uint64) Capacity() int {
+	return cap(s.buffer) / int(s.channels)
 }
 
-func (s Uint64) setSample(channel, pos int, val uint64) {
-	s.buffer[interPos(s.Channels(), channel, pos)] = val
+func (s Uint64) Length() int {
+	return len(s.buffer) / int(s.channels)
 }
 
-func (s Uint64) setLength(l int) Unsigned {
-	s.length = length(l)
+func (s Uint64) Cap() int {
+	return cap(s.buffer)
+}
+
+func (s Uint64) Len() int {
+	return len(s.buffer)
+}
+
+func (s Uint64) Reset() Unsigned {
+	return s.Slice(0, 0)
+}
+
+func (s Uint64) AppendSample(value uint64) Unsigned {
+	if len(s.buffer) == cap(s.buffer) {
+		return s
+	}
+	s.buffer = append(s.buffer, s.BitDepth().UnsignedValue(value))
 	return s
 }
 
-func (s Uint64) Slice(start, end int) Unsigned {
-	s.buffer = s.buffer[interPos(s.Channels(), 0, start):]
-	s.capacity = capacity(s.Capacity() - start)
-	return s.setLength(end - start)
+// Sample returns signal value for provided channel and position.
+func (s Uint64) Sample(pos int) uint64 {
+	return s.buffer[pos]
 }
 
-// Append appends [0:Length] data from src to current buffer and returns new
-// Signed buffer. Both buffers must have same number of channels and bit depth,
+func (s Uint64) SetSample(pos int, value uint64) {
+	s.buffer[pos] = s.BitDepth().UnsignedValue(value)
+}
+
+func (s Uint64) Slice(start, end int) Unsigned {
+	start = s.ChannelPos(0, start)
+	end = s.ChannelPos(0, end)
+	s.buffer = s.buffer[start:end]
+	return s
+}
+
+// Append appends data from src to current buffer and returns new
+// Unsigned buffer. Both buffers must have same number of channels and bit depth,
 // otherwise function will panic. If current buffer doesn't have enough capacity,
 // additional memory will be allocated and result buffer will have capacity and
 // length equal to sum of lengths.
 func (s Uint64) Append(src Unsigned) Unsigned {
 	mustSameChannels(s.Channels(), src.Channels())
 	mustSameBitDepth(s.BitDepth(), src.BitDepth())
-	newLen := s.Length() + src.Length()
-	if s.Capacity() < newLen {
-		s.buffer = append(s.buffer, make([]uint64, (newLen-s.Capacity())*s.Channels())...)
-		s.capacity = capacity(newLen)
+	if s.Cap() < s.Len()+src.Len() {
+		// if capacity is not enough, then:
+		// * extend buffer to cap;
+		// * allocate and append buffer with length of source capacity;
+		// * slice it to current data length;
+		s.buffer = append(s.buffer[:s.Cap()], make([]uint64, src.Cap())...)[:s.Len()]
 	}
+	result := Unsigned(s)
+	for pos := 0; pos < src.Len(); pos++ {
+		result = result.AppendSample(src.Sample(pos))
+	}
+	return result
+}
+
+func ReadStripedUint64(s Unsigned, buf [][]uint64) {
+	mustSameChannels(s.Channels(), len(buf))
 	for channel := 0; channel < s.Channels(); channel++ {
-		for pos := 0; pos < src.Length(); pos++ {
-			s.setSample(channel, pos+s.Length(), src.Sample(channel, pos))
+		for pos := 0; pos < s.Length() && pos < len(buf[channel]); pos++ {
+			buf[channel][pos] = s.Sample(s.ChannelPos(channel, pos))
 		}
 	}
-	s.length = length(newLen)
+}
+
+// WriteUint64 writes values from provided slice into the buffer.
+// If the buffer already contains any data, it will be overwritten.
+// Sample values are capped by maximum value of the buffer bit depth.
+func WriteUint64(s Unsigned, buf []uint64) Unsigned {
+	length := min(s.Cap()-s.Len(), len(buf))
+	for pos := 0; pos < length; pos++ {
+		s = s.AppendSample(buf[pos])
+	}
+	return s
+}
+
+// WriteStripedUint64 writes values from provided slice into the buffer.
+// If the buffer already contains any data, it will be overwritten.
+// The length of provided slice must be equal to the number of channels,
+// otherwise function will panic. Length is set to the longest
+// nested slice length. Sample values are capped by maximum value of
+// the buffer bit depth.
+func WriteStripedUint64(s Unsigned, buf [][]uint64) Unsigned {
+	mustSameChannels(s.Channels(), len(buf))
+	var length int
+	for i := range buf {
+		if len(buf[i]) > length {
+			length = len(buf[i])
+		}
+	}
+	length = min(length, s.Capacity()-s.Length())
+	for pos := 0; pos < length; pos++ {
+		for channel := 0; channel < s.Channels(); channel++ {
+			if pos < len(buf[channel]) {
+				s = s.AppendSample(buf[channel][pos])
+			} else {
+				s = s.AppendSample(0)
+			}
+		}
+	}
 	return s
 }
